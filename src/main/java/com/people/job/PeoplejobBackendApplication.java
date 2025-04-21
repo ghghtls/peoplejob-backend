ef0c7ecd0ebe4e483226de26a771c69dee9d9687
ghghtls <ghghtls2@gmail.com>
2025-04-21 17:01:19 +0900

    Initial commit

---
+++ src/main/java/com/people/job/PeoplejobBackendApplication.java
@@ -0,0 +1,11 @@
+package com.people.job;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeoplejobBackendApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PeoplejobBackendApplication.class, args);
+    }
+}
---
+++ src/test/java/com/people/job/peoplejob_backend/PeoplejobBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.people.job.peoplejob_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeoplejobBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
